6efe558ff304a9ce0839ac3c821b933a92c54897
Marian <mnelugo@gmail.com>
2022-03-29 21:27:49 -0400

    Inicial

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+import './styles/index.css'
+
+render(
+  <StrictMode>
+    <p>BUSCADOR</p>
+  </StrictMode>,
+  document.getElementById('root')
+)
